c2a33cf83c65c6ac008463c19df71d6fd7f827cc
JiMoonGil <roadzmoon76@naver.com>
2022-01-23 14:18:08 +0900

    second commit

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ let indexNum = 1;
 let rightButton = document.querySelector('.right-button');
 let leftButton = document.querySelector('.left-button');
 let mainImage = document.querySelector('.main-image');
-let imageArray = [
-    'images/image-1.png',
-    'images/image-2.png',
-    'images/image-3.png',
-    'images/image-4.png',
-    'images/image-5.png'
-]
 let bottomButton1 = document.querySelector('.bottom-button-1')
 let bottomButton2 = document.querySelector('.bottom-button-2')
 let bottomButton3 = document.querySelector('.bottom-button-3')
@@ -21,38 +14,52 @@ document.querySelector(`.bottom-icon-${indexNum}`).style.opacity = 1;
 rightButton.addEventListener('click', nextImage);
 leftButton.addEventListener('click', previousImage);
 
-console.log(indexNum);
-
-bottomButton1.addEventListener('click', indexImage (1));
-bottomButton2.addEventListener('click', indexImage (2));
-bottomButton3.addEventListener('click', indexImage (3));
-bottomButton4.addEventListener('click', indexImage (4));
-bottomButton5.addEventListener('click', indexImage (5));
-
-console.log(indexNum);
+document.querySelector('.bottom-button-1').addEventListener('click', function () {
+    indexImage(1);
+});
+document.querySelector('.bottom-button-2').addEventListener('click', function () {
+    indexImage(2);
+});
+document.querySelector('.bottom-button-3').addEventListener('click', function () {
+    indexImage(3);
+});
+document.querySelector('.bottom-button-4').addEventListener('click', function () {
+    indexImage(4);
+});
+document.querySelector('.bottom-button-5').addEventListener('click', function () {
+    indexImage(5);
+});
 
 function nextImage () {
+    document.querySelector(`.bottom-icon-${indexNum}`).style.opacity = 0.3;
     indexNum ++;
 
     if (indexNum === 6) {
         indexNum = 1;
     }
 
+    document.querySelector(`.bottom-icon-${indexNum}`).style.opacity = 1;
     mainImage.src = `images/image-${indexNum}.png`;
 }
 
 function previousImage () {
+    document.querySelector(`.bottom-icon-${indexNum}`).style.opacity = 0.3;
     indexNum --;
 
     if (indexNum === 0) {
         indexNum = 5;
     }
 
+    document.querySelector(`.bottom-icon-${indexNum}`).style.opacity = 1;
     mainImage.src = `images/image-${indexNum}.png`;
 }
 
-function indexImage (i) {
-    indexNum = i;
-    mainImage.src = `images/image-${i}.png`;
-    document.querySelector(`.bottom-icon-${i}`).style.opacity = 1;
+function indexImage (j) {
+    for (let i = 1; i <= 5; i++) {
+        document.querySelector(`.bottom-icon-${i}`).style.opacity = 0.3;
+    }
+    
+    indexNum = j;
+    mainImage.src = `images/image-${j}.png`;
+    document.querySelector(`.bottom-icon-${j}`).style.opacity = 1;
 }
